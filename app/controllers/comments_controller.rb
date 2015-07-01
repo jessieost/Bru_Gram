04a986d0ae39f304f54c3b074dbc2f46d9e1e786
jessieost <jessie.osterman@gmail.com>
2015-07-01 22:35:20 +0200

    added bootstrap theme, kinda jank but figure out later

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,7 +17,8 @@ class CommentsController < ApplicationController
 
 	def edit
 		@photo = Photo.find params[:photo_id]
-		@comment = Comment.find params[:id]		
+		@comment = Comment.find params[:id]	
+
 	end
 
 	def update
@@ -29,6 +30,7 @@ class CommentsController < ApplicationController
 			render 'edit'
 		end
 	end
+	
 	private
 		def comment_params
 			params.require(:comment).permit(:body)
